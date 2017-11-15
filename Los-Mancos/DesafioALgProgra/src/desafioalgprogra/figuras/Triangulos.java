04032ad9d216d5b6c34693fc170064198ad27aad
JPJara <33356026+JPJara@users.noreply.github.com>
2017-11-15 08:47:43 -0300

    arreglo de un pequeño error en una opción

--- Los-Mancos/DesafioALgProgra/src/desafioalgprogra/figuras/Triangulos.java
+++ Los-Mancos/DesafioALgProgra/src/desafioalgprogra/figuras/Triangulos.java
@@ -1,7 +1,5 @@
 package desafioalgprogra.figuras;
 
-import java.util.Scanner;
-
 /**
  *
  * @author Ratita
@@ -9,25 +7,23 @@ import java.util.Scanner;
 public class Triangulos {
 
     public int datosx(String punto) {
-        
+
         String cortar[] = punto.split(",");
 
         int ejeX = Integer.parseInt(cortar[0]);
         int ejeY = Integer.parseInt(cortar[1]);
         return ejeX;
-        
-        }
+
+    }
+
     public int datosy(String punto) {
-        
+
         String cortar[] = punto.split(",");
 
         int ejeX = Integer.parseInt(cortar[0]);
         int ejeY = Integer.parseInt(cortar[1]);
         return ejeY;
-        
-        }
-        
-        
+
     }
 
-
+}
